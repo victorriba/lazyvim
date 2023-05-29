5406a4da50f9064159b1918c608c0a029f288291
Victor Riveros <victorriba18@hotmail.com>
2023-05-29 00:22:53 -0500

    first commit

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,24 @@
+return {
+  "akinsho/toggleterm.nvim",
+  keys = {
+    { "<leader>tf", "<Cmd>2ToggleTerm<Cr>", desc = "Terminal Float" },
+    { "<leader>tv", "<Cmd>3ToggleTerm direction=vertical<Cr>", desc = "Terminal Vertical" },
+  },
+  cmd = { "ToggleTerm", "TermExec" },
+  opts = {
+    size = 20,
+    hide_numbers = true,
+    shade_filetypes = {},
+    shade_terminals = false,
+    shading_factor = 0.3,
+    start_in_insert = true,
+    persist_size = true,
+    direction = "float",
+    winbar = {
+      enabled = false,
+      name_formatter = function(term)
+        return term.name
+      end,
+    },
+  },
+}
